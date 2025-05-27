71ab4a7aa6ac9862b3be1b07f5c606c141a02464
yunajoe <yunajoe@gmail.com>
2025-05-27 21:02:35 +0900

    feat: modal완성

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,78 @@
-import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs, useRouter } from "expo-router";
 
 export default function TabLayout() {
+  const router = useRouter();
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="search" />
-      <Tabs.Screen name="addtest" />
-      <Tabs.Screen name="activity" />
-      <Tabs.Screen name="[username]" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          // 아이콘 아래에 title 안보이게 함
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="home"
+              size={24}
+              color={focused ? "black" : "gray"}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="search"
+              size={24}
+              color={focused ? "black" : "gray"}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="add"
+        listeners={{
+          tabPress: (e) => {
+            e.preventDefault();
+            router.navigate("/modal");
+          },
+        }}
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons name="add" size={24} color={focused ? "black" : "gray"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="activity"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="heart-outline"
+              size={24}
+              color={focused ? "black" : "gray"}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="[username]"
+        options={{
+          tabBarLabel: () => null,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name="person-outline"
+              size={24}
+              color={focused ? "black" : "gray"}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,14 @@
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 
 // slot
+// presentation 은 stack만 된다.
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="modal"
+        options={{ presentation: "modal" }}
+      ></Stack.Screen>
+    </Stack>
+  );
 }
